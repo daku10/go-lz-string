a84e422b389aef3f7932edd3c818c599a1636fac
daku10 <daku.two@gmail.com>
2022-03-21 21:45:37 +0900

    tmp

--- lzstring.go
+++ lzstring.go
@@ -9,3 +9,34 @@ func Decompress() {
 func Compress(reader io.Reader) (io.Reader, error) {
 	return reader, nil
 }
+
+type GetCharFunc func(i int) string
+
+func compressCore(uncompressed string, bitsPerChar int, getCharFromInt GetCharFunc) (string, error) {
+	var i, value int
+	contextDictionary := make(map[string]int)
+	contextDictionaryToCreate := make(map[string]bool)
+	var contextC, contextWC, contextW string
+	contextEnLargeIn := 2
+	contextDictSize := 3
+	contextNumBits := 2
+	contextData := make([]string, 0)
+	contextDataVal := 0
+	contextDataPosition := 0
+	var ii int
+	uncompressedRune := []rune(uncompressed)
+	for ii := 0; ii < len(uncompressedRune); ii++ {
+		contextC := string(uncompressedRune[ii])
+		if _, ok := contextDictionary[contextC]; !ok {
+			contextDictionary[contextC] = contextDictSize
+			contextDictSize++
+			contextDictionaryToCreate[contextC] = true
+		}
+		contextWC = contextW + contextC
+		if _, ok := contextDictionary[contextWC]; ok {
+			contextW = contextWC
+		} else {
+
+		}
+	}
+}
