1288710268c283ad12b2f61db4ceea4f8ca6f00b
daku10 <daku.two@gmail.com>
2022-03-27 15:12:20 +0900

    add Decompress

--- lzstring.go
+++ lzstring.go
@@ -8,8 +8,8 @@ import (
 	"unicode/utf16"
 )
 
-func Decompress() {
-
+func f(i int) string {
+	return string([]rune{rune(i)})
 }
 
 func Compress(reader io.Reader) (string, error) {
@@ -234,4 +234,211 @@ func _compress(uncompressed string, bitsPerChar int, getCharFromInt GetCharFunc)
 		}
 	}
 	return strings.Join(contextData, ""), nil
+}
+
+func Decompress(compressed string) string {
+	if compressed == "" {
+		return ""
+	}
+	compressedRune := utf16.Encode([]rune(compressed))
+	return _decompress(len(compressedRune), 32768, func(index int) int {
+		return int(compressedRune[index])
+	})
+}
+
+type GetNextValFunc = func(index int) int
+
+type Data struct {
+	val      int
+	position int
+	index    int
+}
+
+func _decompress(length int, resetValue int, getNextVal GetNextValFunc) string {
+	// for init
+	dictionary := make(map[any]any)
+	var next int
+	enlargeIn := 4
+	dictSize := 4
+	numBits := 3
+	entry := ""
+	result := make([]string, 0)
+	var i, bits, resb, maxpower, power int
+	var w, c interface{}
+	data := Data{val: getNextVal(0), position: resetValue, index: 1}
+
+	for i = 0; i < 3; i++ {
+		dictionary[i] = i
+	}
+	bits = 0
+	maxpower = int(math.Pow(2, 2))
+	power = 1
+	for power != maxpower {
+		resb = data.val & data.position
+		data.position >>= 1
+		if data.position == 0 {
+			data.position = resetValue
+			data.val = getNextVal(data.index)
+			data.index += 1
+		}
+		tmp := 0
+		if resb > 0 {
+			tmp = 1
+		}
+		bits |= tmp * power
+		power <<= 1
+	}
+	next = bits
+	switch next {
+	case 0:
+		bits = 0
+		maxpower = int(math.Pow(2, 8))
+		power = 1
+		for power != maxpower {
+			resb = data.val & data.position
+			data.position >>= 1
+			if data.position == 0 {
+				data.position = resetValue
+				data.val = getNextVal(data.index)
+				data.index += 1
+			}
+			tmp := 0
+			if resb > 0 {
+				tmp = 1
+			}
+			bits |= tmp * power
+			power <<= 1
+		}
+		c = f(bits)
+	case 1:
+		bits = 0
+		maxpower = int(math.Pow(2, 16))
+		power = 1
+		for power != maxpower {
+			resb = data.val & data.position
+			data.position >>= 1
+			if data.position == 0 {
+				data.position = resetValue
+				data.val = getNextVal(data.index)
+				data.index += 1
+			}
+			tmp := 0
+			if resb > 0 {
+				tmp = 1
+			}
+			bits |= tmp * power
+			power <<= 1
+		}
+		c = f(bits)
+		break
+	case 2:
+		return ""
+	}
+	dictionary[3] = c
+	w = c
+	result = append(result, c.(string))
+	for {
+		if data.index > length {
+			return ""
+		}
+
+		bits = 0
+		maxpower = int(math.Pow(2, float64(numBits)))
+		power = 1
+		for power != maxpower {
+			resb = data.val & data.position
+			data.position >>= 1
+			if data.position == 0 {
+				data.position = resetValue
+				data.val = getNextVal(data.index)
+				data.index += 1
+			}
+			tmp := 0
+			if resb > 0 {
+				tmp = 1
+			}
+			bits |= tmp * power
+			power <<= 1
+		}
+
+		c = bits
+		switch c {
+		case 0:
+			bits = 0
+			maxpower = int(math.Pow(2, 8))
+			power = 1
+			for power != maxpower {
+				resb = data.val & data.position
+				data.position >>= 1
+				if data.position == 0 {
+					data.position = resetValue
+					data.val = getNextVal(data.index)
+					data.index++
+				}
+				tmp := 0
+				if resb > 0 {
+					tmp = 1
+				}
+				bits |= tmp * power
+				power <<= 1
+			}
+
+			dictionary[dictSize] = f(bits)
+			dictSize++
+			c = dictSize - 1
+			enlargeIn--
+		case 1:
+			bits = 0
+			maxpower = int(math.Pow(2, 16))
+			power = 1
+			for power != maxpower {
+				resb = data.val & data.position
+				data.position >>= 1
+				if data.position == 0 {
+					data.position = resetValue
+					data.val = getNextVal(data.index)
+					data.index++
+				}
+				tmp := 0
+				if resb > 0 {
+					tmp = 1
+				}
+				bits |= tmp * power
+				power <<= 1
+			}
+			dictionary[dictSize] = f(bits)
+			dictSize++
+			c = dictSize - 1
+			enlargeIn--
+		case 2:
+			return strings.Join(result, "")
+		}
+
+		if enlargeIn == 0 {
+			enlargeIn = int(math.Pow(2, float64(numBits)))
+			numBits++
+		}
+
+		if _, ok := dictionary[c]; ok {
+			entry = dictionary[c].(string)
+		} else {
+			if c == dictSize {
+				entry = w.(string) + string([]rune(w.(string))[0])
+			} else {
+				return ""
+			}
+		}
+		result = append(result, entry)
+
+		dictionary[dictSize] = w.(string) + string([]rune(entry)[0])
+		dictSize++
+		enlargeIn--
+
+		w = entry
+
+		if enlargeIn == 0 {
+			enlargeIn = int(math.Pow(2, float64(numBits)))
+			numBits++
+		}
+	}
 }
