10db9a0e4b42a04677659074fec2609ba0dee145
daku10 <daku.two@gmail.com>
2022-03-27 15:12:34 +0900

    add fail test

--- lzstring_test.go
+++ lzstring_test.go
@@ -44,6 +44,10 @@ func TestCompress(t *testing.T) {
 			arg:  "aあ🍎bい🍇c",
 			want: string([]rune{0x21a2, 0x1064, 0x3c1b, 0x872f, 0xb046, 0x8220, 0xc68e, 0x2fb0, 0x6320}),
 		},
+		{
+			arg:  string([]rune{0x9c}),
+			want: string([]rune{0xe50}),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.arg, func(t *testing.T) {
@@ -56,5 +60,61 @@ func TestCompress(t *testing.T) {
 }
 
 func TestDecompress(t *testing.T) {
+	tests := []struct {
+		arg  string
+		want string
+	}{
+		{
+			arg:  "Ґ",
+			want: "H",
+		},
+		{
+			arg:  string([]rune{0x485, 0x3036, 0x60f6, 0xa194, 0x0}),
+			want: "HelloHello",
+		},
+		{
+			arg:  string([]rune{0x2182, 0x3518, 0xc204, 0xda14, 0xc800}),
+			want: "ababcabcdabcde",
+		},
+		{
+			arg:  "҅〶惶̀Ў㦀♀",
+			want: "Hello, world",
+		},
+		{
+			arg:  "邃ℐ摢ಁ⃈刌䀀",
+			want: "あいうえお",
+		},
+		{
+			arg:  string([]rune{'輆', '', 0xda00}),
+			want: "🍎",
+		},
+		{
+			arg:  string([]rune{0x8f06, 0xe397, 0xde9c, 0x5f68}),
+			want: "🍎🍇",
+		},
+		{
+			arg:  string([]rune{0x21a2, 0x1064, 0x3c1b, 0x872f, 0xb046, 0x8220, 0xc68e, 0x2fb0, 0x6320}),
+			want: "aあ🍎bい🍇c",
+		},
+		{
+			arg:  string([]rune{0x9c}),
+			want: string([]rune{0xe50}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.arg, func(t *testing.T) {
+			res := Decompress(tt.arg)
+			assert.Equal(t, tt.want, res)
+		})
+	}
+}
 
+func FuzzIntegrity(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		t.Log("aaa", []byte(s))
+		compressed, err := Compress(strings.NewReader(s))
+		assert.Nil(t, err)
+		repair := Decompress(compressed)
+		assert.Equal(t, s, repair)
+	})
 }
