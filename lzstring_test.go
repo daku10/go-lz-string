3cf70cca7a45570a44acf9cc9117f9d78c89e53d
daku10 <daku.two@gmail.com>
2022-03-29 00:42:03 +0900

    add fuzz test for Uint8Array, EncodedURIComponent

--- lzstring_test.go
+++ lzstring_test.go
@@ -671,3 +671,41 @@ func FuzzIntegrityUTF16(f *testing.F) {
 		assert.Equal(t, s, repair)
 	})
 }
+
+func FuzzIntegrityUint8Array(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		compressed, err := CompressToUint8Array(s)
+		if !utf8.ValidString(s) {
+			assert.NotNil(t, err)
+			return
+		}
+		repair, err := DecompressFromUint8Array(compressed)
+		assert.Nil(t, err)
+		assert.Equal(t, s, repair)
+	})
+}
+
+func FuzzIntegrityEncodedURIComponent(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		compressed, err := CompressToEncodedURIComponent(s)
+		if !utf8.ValidString(s) {
+			assert.NotNil(t, err)
+			return
+		}
+		if !utf8.ValidString(s) {
+			assert.NotNil(t, err)
+			return
+		}
+		assert.Condition(t, func() (success bool) {
+			for _, c := range compressed {
+				if !strings.Contains(keyStrUriSafe, string(c)) {
+					return false
+				}
+			}
+			return true
+		})
+		repair, err := DecompressFromEncodedURIComponent(compressed)
+		assert.Nil(t, err)
+		assert.Equal(t, s, repair)
+	})
+}
