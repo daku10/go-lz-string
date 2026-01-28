d80983bfa743cae3889aa6fd610d6cb24b6d8d52
daku10 <daku10.dev@gmail.com>
2026-01-28 15:53:39 +0900

    chore: handle golangci-lint

--- cli/version.go
+++ cli/version.go
@@ -2,7 +2,6 @@ package cli
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 
 	"github.com/daku10/go-lz-string/version"
@@ -16,9 +15,12 @@ func newVersionCmd(config *Config) *cobra.Command {
 		Long:  "print the version of go-lz-string",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			bufWriter := bufio.NewWriter(cmd.OutOrStdout())
-			fmt.Fprintln(bufWriter, version.Version)
+			_, err := fmt.Fprintln(bufWriter, version.Version)
+			if err != nil {
+				return fmt.Errorf("version command failed: %w", err)
+			}
 			if err := bufWriter.Flush(); err != nil {
-				return errors.New("version command failed")
+				return fmt.Errorf("version command failed: %w", err)
 			}
 			return nil
 		},
